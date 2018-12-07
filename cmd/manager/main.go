144eb9a27a1dd8cecc74b451e4bf8bf65f6dfa59
Barni S <barni@google.com>
2018-12-07 17:57:20 -0500

    reconcile every 2 mins

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -26,6 +26,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 	"sigs.k8s.io/controller-runtime/pkg/runtime/signals"
+	"time"
 )
 
 func main() {
@@ -41,8 +42,9 @@ func main() {
 	}
 
 	// Create a new Cmd to provide shared dependencies and start components
+	syncperiod := time.Minute * 2
 	log.Info("setting up manager")
-	mgr, err := manager.New(cfg, manager.Options{})
+	mgr, err := manager.New(cfg, manager.Options{SyncPeriod: &syncperiod})
 	if err != nil {
 		log.Error(err, "unable to set up overall controller manager")
 		os.Exit(1)
