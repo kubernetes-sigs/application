f4ab6d54f332b88e820027078b73387876e84b16
Richard Liu <ricliu@google.com>
2019-10-22 14:52:52 -0700

    Generate client for application type

---
+++ hack/update-codegen.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Copyright 2019 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This shell is used to auto generate some useful tools for k8s, such as lister,
+# informer, deepcopy, defaulter and so on.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
+CODEGEN_PKG=${SCRIPT_ROOT}/vendor/k8s.io/code-generator
+
+# generate the code with:
+# --output-base    because this script should also be able to run inside the vendor dir of
+#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
+#                  instead of the $GOPATH directly. For normal projects this can be dropped.
+cd ${SCRIPT_ROOT}
+
+${CODEGEN_PKG}/generate-groups.sh "all" \
+ github.com/kubernetes-sigs/application/pkg/client github.com/kubernetes-sigs/application/pkg/apis \
+ app:v1beta1 \
+ --go-header-file hack/boilerplate.go.txt
