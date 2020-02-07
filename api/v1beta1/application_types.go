edb516e147a3715eb6ba7266c97dfa6ff43087ce
Barni S <barni@google.com>
2020-02-07 14:21:47 -0500

    Add protocol fields to Service and Ingress info items

--- api/v1beta1/application_types.go
+++ api/v1beta1/application_types.go
@@ -272,6 +272,8 @@ type ServiceSelector struct {
 	Port *int32 `json:"port,omitempty"`
 	// The optional HTTP path.
 	Path string `json:"path,omitempty"`
+	// Protocol for the service
+	Protocol string `json:"protocol,omitempty"`
 }
 
 // IngressSelector selects an Ingress.
@@ -282,6 +284,8 @@ type IngressSelector struct {
 	Host string `json:"host,omitempty"`
 	// The optional HTTP path.
 	Path string `json:"path,omitempty"`
+	// Protocol for the ingress
+	Protocol string `json:"protocol,omitempty"`
 }
 
 // ApplicationAssemblyPhase tracks the Application CRD phases: pending, succeded, failed
@@ -326,7 +330,7 @@ func init() {
 	SchemeBuilder.Register(&Application{}, &ApplicationList{})
 }
 
-// Strip the version part of gv
+// StripVersion the version part of gv
 func StripVersion(gv string) string {
 	if gv == "" {
 		return gv
