fa8700f94776525edca9e27372bdc6b4c5a903be
Nan Yu <nanyu@google.com>
2020-02-12 17:14:53 -0800

    Add a more comprehensive test with custom resources

--- e2e/main_test.go
+++ e2e/main_test.go
@@ -17,6 +17,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	"github.com/onsi/ginkgo/reporters"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 	apiextcs "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -29,6 +30,7 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	appv1beta1 "sigs.k8s.io/application/api/v1beta1"
+	"sigs.k8s.io/application/controllers"
 	"sigs.k8s.io/application/e2e/testutil"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -54,6 +56,9 @@ func getKubeClientOrDie(config *rest.Config, s *runtime.Scheme) client.Client {
 const (
 	crdPath         = "../config/crd/bases/app.k8s.io_applications.yaml"
 	applicationPath = "../config/samples/app_v1beta1_application.yaml"
+	waitTimeout     = time.Second * 120
+	pullPeriod      = time.Second * 2
+	syncPeriod      = "2"
 )
 
 var _ = Describe("Application CRD e2e", func() {
@@ -77,7 +82,7 @@ var _ = Describe("Application CRD e2e", func() {
 
 	var managerStdout bytes.Buffer
 	var managerStderr bytes.Buffer
-	managerCmd := exec.Command("../bin/manager", "--sync-period", "30")
+	managerCmd := exec.Command("../bin/manager", "--sync-period", syncPeriod)
 	managerCmd.Stdout = &managerStdout
 	managerCmd.Stderr = &managerStderr
 
@@ -110,16 +115,34 @@ var _ = Describe("Application CRD e2e", func() {
 		Expect(err).NotTo(HaveOccurred())
 	})
 
-	It("should update application status", func() {
+	It("should create the test application with custom resources", func() {
+		err = kubeApply("../docs/examples/test_app/test_crd.yaml")
+		Expect(err).NotTo(HaveOccurred())
+		err = kubeApply("../docs/examples/test_app/application.yaml")
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("should update wordpress-01 status", func() {
 		kubeClient := getKubeClientOrDie(config, s)
 		application := &appv1beta1.Application{}
 		objectKey := types.NamespacedName{
 			Namespace: metav1.NamespaceDefault,
 			Name:      "wordpress-01",
 		}
-		waitForApplicationStatusUpdated(kubeClient, objectKey, application)
+		waitForApplicationStatusToHaveNComponents(kubeClient, objectKey, application, 5)
 		Expect(application.Status.ObservedGeneration).To(BeNumerically("<=", 5))
 		Expect(application.Status.ComponentList.Objects).To(HaveLen(5))
+	})
+
+	It("should update test-application-01 status", func() {
+		kubeClient := getKubeClientOrDie(config, s)
+		application := &appv1beta1.Application{}
+		objectKey := types.NamespacedName{
+			Namespace: metav1.NamespaceDefault,
+			Name:      "test-application-01",
+		}
+		waitForApplicationStatusToHaveNComponents(kubeClient, objectKey, application, 7)
+		Expect(application.Status.ObservedGeneration).To(BeNumerically("<=", 7))
 	})
 
 	It("should add ownerReference to components", func() {
@@ -131,13 +154,35 @@ var _ = Describe("Application CRD e2e", func() {
 			Group: "",
 			Kind:  "Service",
 		})
-		validateComponentOwnerReferences(kubeClient, list, matchingLabels)
+		validateComponentOwnerReferences(kubeClient, list, matchingLabels, "wordpress-01")
 
 		list.SetGroupVersionKind(schema.GroupVersionKind{
 			Group: "apps",
 			Kind:  "StatefulSet",
 		})
-		validateComponentOwnerReferences(kubeClient, list, matchingLabels)
+		validateComponentOwnerReferences(kubeClient, list, matchingLabels, "wordpress-01")
+
+		matchingLabels = map[string]string{"app.kubernetes.io/name": "test-01"}
+		list.SetGroupVersionKind(schema.GroupVersionKind{
+			Group: "test.crd.com",
+			Kind:  "TestCRD",
+		})
+		validateComponentOwnerReferences(kubeClient, list, matchingLabels, "test-application-01")
+	})
+
+	It("should mark the application not-ready if not all components are ready", func() {
+		err = kubeApply("app-with-bad-deployment.yaml")
+		Expect(err).NotTo(HaveOccurred())
+
+		kubeClient := getKubeClientOrDie(config, s)
+		application := &appv1beta1.Application{}
+		objectKey := types.NamespacedName{
+			Namespace: metav1.NamespaceDefault,
+			Name:      "app-with-bad-deployment",
+		}
+		waitForApplicationStatusToHaveNComponents(kubeClient, objectKey, application, 4)
+		Expect(application.Status.ObservedGeneration).To(BeNumerically("<=", 4))
+		Expect(hasConditionTypeStatusAndReason(application.Status.Conditions, controllers.StatusReady, corev1.ConditionFalse, "ComponentsNotReady")).To(BeTrue())
 	})
 
 	It("should stop the controller", func() {
@@ -153,37 +198,41 @@ var _ = Describe("Application CRD e2e", func() {
 	})
 })
 
-func validateComponentOwnerReferences(kubeClient client.Client, list *unstructured.UnstructuredList, matchedingLabels map[string]string) {
-	componentsUpdated := false
-	_ = wait.PollImmediate(time.Second, time.Second*30, func() (bool, error) {
+func validateComponentOwnerReferences(kubeClient client.Client, list *unstructured.UnstructuredList, matchedingLabels map[string]string, ownerName string) {
+	err := wait.PollImmediate(pullPeriod, waitTimeout, func() (bool, error) {
 
+		log.Println("Pulling the component with Kind = ", list.GetKind())
 		if err := kubeClient.List(context.TODO(), list, client.InNamespace(metav1.NamespaceDefault), client.MatchingLabels(matchedingLabels)); err != nil {
-			return false, err
+			return false, nil
 		}
 
-		updated := true
 		for _, item := range list.Items {
-			if item.GetOwnerReferences() == nil || len(item.GetOwnerReferences()) < 1 || item.GetOwnerReferences()[0].Name != "wordpress-01" {
-				updated = false
+			if item.GetOwnerReferences() == nil || len(item.GetOwnerReferences()) < 1 || item.GetOwnerReferences()[0].Name != ownerName {
+				log.Println("Component ownerReferences has NOT been updated yet")
+				return false, nil
 			}
 		}
-		componentsUpdated = updated
-		return updated, nil
+		log.Println("Component ownerReferences has been updated successfully")
+		return true, nil
 	})
-	Expect(componentsUpdated).To(BeTrue())
+	Expect(err).NotTo(HaveOccurred())
 }
 
-func waitForApplicationStatusUpdated(kubeClient client.Client, key client.ObjectKey, app *appv1beta1.Application) {
-	_ = wait.PollImmediate(time.Second, time.Second*180, func() (bool, error) {
+func waitForApplicationStatusToHaveNComponents(kubeClient client.Client, key client.ObjectKey, app *appv1beta1.Application, n int) {
+	err := wait.PollImmediate(pullPeriod, waitTimeout, func() (bool, error) {
+		log.Println("Pulling the application status")
 		if err := kubeClient.Get(context.TODO(), key, app); err != nil {
-			return false, err
+			return false, nil
 		}
 
-		if app.Status.ComponentList.Objects != nil && len(app.Status.ComponentList.Objects) == 5 && app.Status.Conditions != nil {
+		if app.Status.ComponentList.Objects != nil && len(app.Status.ComponentList.Objects) == n && app.Status.Conditions != nil {
+			log.Println("Application status has been updated successfully")
 			return true, nil
 		}
+		log.Println("Application status has NOT been updated yet")
 		return false, nil
 	})
+	Expect(err).NotTo(HaveOccurred())
 }
 
 func applyKustomize(path string) error {
@@ -224,4 +273,20 @@ func applyKustomize(path string) error {
 	_, _ = io.Copy(os.Stdout, &kubectlOP)
 
 	return nil
+}
+
+func kubeApply(path string) error {
+	kubectl := exec.Command("../hack/tools/bin/kubectl", "apply", "-f", path)
+	out, err := kubectl.CombinedOutput()
+	log.Println(string(out))
+	return err
+}
+
+func hasConditionTypeStatusAndReason(conditions []appv1beta1.Condition, t appv1beta1.ConditionType, s corev1.ConditionStatus, r string) bool {
+	for _, condition := range conditions {
+		if condition.Type == t && condition.Status == s && condition.Reason == r {
+			return true
+		}
+	}
+	return false
 }
